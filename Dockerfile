020faeab2eb2cb6413fbbfffeaa0bec3535847df
manishthakur <mt86techlearning@gmail.com>
2023-12-30 12:59:42 +0000

    Initial Commit

---
+++ Dockerfile
@@ -0,0 +1,29 @@
+FROM python:3.11-slim-buster
+
+RUN apt-get -y update && apt-get install -y --no-install-recommends \
+         wget \
+         python3 \
+         nginx \
+         ca-certificates \
+    && rm -rf /var/lib/apt/lists/*
+
+RUN wget https://bootstrap.pypa.io/get-pip.py && python3 get-pip.py && \
+    pip install flask gevent gunicorn && \
+        rm -rf /root/.cache
+
+WORKDIR /opt/program
+
+COPY meta-ai-seamless /opt/program
+COPY requirements.txt /opt/program/requirements.txt
+RUN pip install --no-cache-dir -r requirements.txt
+
+
+# Set some environment variables. PYTHONUNBUFFERED keeps Python from buffering our standard
+# output stream, which means that logs can be delivered to the user quickly. PYTHONDONTWRITEBYTECODE
+# keeps Python from writing the .pyc files which are unnecessary in this case. We also update
+# PATH so that the train and serve programs are found when the container is invoked.
+ENV PYTHONUNBUFFERED=TRUE \
+    PYTHONDONTWRITEBYTECODE=TRUE \
+    PYTHONPATH="/opt/program:${PATH}"
+
+CMD ["python", "serve.py"]
---
+++ meta-ai-seamless/predictor.py
@@ -0,0 +1,51 @@
+import json
+import logging
+
+import torch
+from flask import Flask, Response, request
+from transformers import SeamlessM4Tv2Model
+
+from postprocessor import Postprocessor
+from preprocessor import Preprocessor
+
+app = Flask(__name__)
+
+seamless_model = SeamlessM4Tv2Model.from_pretrained("facebook/seamless-m4t-v2-large")
+device = "cuda:0" if torch.cuda.is_available() else "cpu"
+seamless_model.to(device)
+
+
+@app.route('/ping', methods=['GET'])
+def ping():
+    # Check if the model was loaded successfully
+    health = seamless_model is not None
+    status = 200 if health else 404
+    return Response(response='pong', status=status, mimetype='application/json')
+
+
+@app.route('/invocations', methods=['POST'])
+def transformation():
+    data = None
+    if request.content_type == 'application/json':
+        data = request.json
+        logging.info(f"Received data: {data}")
+    else:
+        return Response(response='This predictor only supports JSON data', status=415, mimetype='text/plain')
+
+    # Perform the transformation
+    audio_input = Preprocessor().preprocess(data['audio_file_s3_location'])
+    translated_output = seamless_model.generate(**audio_input, tgt_lang="hin", speaker_id=17)[0].cpu().numpy().squeeze()
+
+    # Post-processing
+    # Write translated output to S3
+    # Return S3 location of translated output
+    translated_audio_message_s3_location = Postprocessor().postprocess(translated_output,
+                                                                       data['audio_file_s3_location'])
+
+    output = {
+        "translated_audio_s3_location": translated_audio_message_s3_location,
+        "translated_text": "This is a test message"
+    }
+
+    # Return JSON response
+    return Response(response=json.dumps(output), status=200, mimetype='application/json')
